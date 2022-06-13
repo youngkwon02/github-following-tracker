2d5dece91047d8551304bfc605bc2a7291e4aec5
youngkwon02 <youngkwon02@naver.com>
2022-06-13 14:48:21 +0900

    chore: seperate fetchGET to util function

---
+++ util/fetchGET.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+const fetchGET = async (reqUrl) => {
+  try {
+    return await axios.get(reqUrl);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  fetchGET,
+};
