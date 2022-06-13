98817ab46ced3bb759189bd0a2f8f3885ceafdd4
youngkwon02 <youngkwon02@naver.com>
2022-06-13 14:18:11 +0900

    feat: crawling functionally works

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { fetchGET } = require("./fetchGET");
+const { getRequestUrl } = require("./util/getRequestUrl");
 const { githubHost } = require("./util/githubHost");
 const { pageType } = require("./util/pageType");
+const { stringToHtml } = require("./util/stringToHtml");
+const axios = require("axios");
 const prompt = require("prompt-sync")({ sigint: true });
 
 // Step 1
@@ -15,6 +17,37 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 // console.log(str);
 
-const githubName = prompt("What is your GitHub name?");
+const fetchGET = async (reqUrl) => {
+  try {
+    return await axios.get(reqUrl);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const GITHUB_HOST = githubHost(githubName);
+const crawling = async () => {
+  // const githubName = prompt("What is your GitHub name? ");
+  const githubName = "youngkwon02";
+  const GITHUB_HOST = githubHost(githubName);
+  let follwingList = [];
+  let follwerList = [];
+
+  const requestUrl = getRequestUrl(GITHUB_HOST, "", {
+    page: 1,
+    tab: pageType.FOLLOWING,
+  });
+
+  let res = await fetchGET(requestUrl);
+  const jsDomRes = stringToHtml(res.data);
+
+  const nameTagList = jsDomRes.window.document.querySelectorAll(
+    ".d-table-cell .Link--secondary"
+  );
+
+  console.log(nameTagList);
+
+  nameTagList.forEach((n) => follwingList.push(n.textContent));
+  console.log(follwingList);
+};
+
+crawling();
